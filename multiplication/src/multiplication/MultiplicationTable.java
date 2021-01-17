0713293a2569f60f5fe8c71b087a5d6ab3172e0c
js <hjsoo@evolvesoft.co.kr>
2021-01-17 16:05:24 +0900

    r1

--- multiplication/src/multiplication/MultiplicationTable.java
+++ multiplication/src/multiplication/MultiplicationTable.java
@@ -6,10 +6,10 @@ public class MultiplicationTable {
 
 	static void level2 () {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("숫자를 입력하세요 : ");
+		System.out.println("plz enter the number : ");
 		int j = sc.nextInt();
 		
-		for(int i = 1; i<=9; i++) {
+		for(int i = 1; i<=10; i++) {
 			int k = i*j;
 			System.out.println(j + " * " + i + " = " + k);
 		}
